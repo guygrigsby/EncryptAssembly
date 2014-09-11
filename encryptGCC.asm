45293a0349a40577ce887ba7c7cc9bd331dc11d9
Guy <guy.grigsby@gmail.com>
2014-09-10 23:14:55 -0600

    reused a single routine

--- encryptGCC.asm
+++ encryptGCC.asm
@@ -1,5 +1,4 @@
 SECTION .bss
-  cText:      resq  1  ; reserve 16 bytes for cypher text
   left2:      resq  1  ; reserved 16 bytes for l2
   right2:     resq  1  ; reserved 16 bytes for r2 
 
@@ -12,7 +11,8 @@ SECTION .data
   key3:       dq    0xE2468AC0
   delta1:     dq    0x11111111
   delta2:     dq    0x22222222
-  pFormat:    db    "C  = %#x", 10, 0
+  pFormat:    db    "%#x", 10, 0
+  encrypt:    dq    0x1
   mask:       dq    0x00000000FFFFFFFF
 SECTION .text
   global main
@@ -26,7 +26,8 @@ main:
   mov r11, [delta1]     ;
   mov r13, [left0]           ;
   mov rax, left2
-  call _encrypt_half
+  mov rbx, [encrypt]
+  call _do_half
 
   mov r8, [left2]          ;
   mov r9, [key2]        ;
@@ -34,27 +35,30 @@ main:
   mov r11, [delta2]     ;
   mov r13, [right0]           ;
   mov rax, right2
-  call _encrypt_half
+  mov rbx, [encrypt]
+  call _do_half
 
   mov r8, [left2]          ;
   mov r9, [key2]        ;
   mov r10, [key3]       ;
   mov r11, [delta2]     ;
   mov r13, [right2]           ;
-  call _decrypt_half
+  xor rbx, rbx
+  call _do_half
 
   mov r8, [right0]          ;
   mov r9, [key0]        ;
   mov r10, [key1]       ;
   mov r11, [delta1]     ;
   mov r13, [left2]           ;
-  call _decrypt_half
+  xor rbx, rbx
+  call _do_half
   ; Terminate program
   mov rax,1            ; 'exit' system call
   mov rbx,0            ; exit with error code 0
   int 80h              ; call the kernel
 
-_decrypt_half:
+_do_half:
   mov r12, r8
   shl r8, 4             ; shift left by 4
   add r9, r8            ; add key to msg << 4
@@ -72,35 +76,18 @@ _decrypt_half:
   xor r11, r8           ; 
 
   mov r8, r13           ; other half of message
-  sub r8, r11           ; this is decrypted 
-  and r8, r15           ; mask most sig 32 bits
-  mov rdi, pFormat      ; prinf format
-  mov rsi, r8           ; what to print
-  xor rax, rax          ; printf uses rax for something
-  call printf           
-  ret
-
-_encrypt_half:
-  mov r12, r8
-  shl r8, 4             ; shift left by 4
-  add r9, r8            ; add key to msg << 4
-  and r9, r15           ; mask most sig 32 bits
-
-  add r11, r12          ; msg + delta
-  and r11, r15          ; mask most sig 32 bits
-
-  mov r8, r12           ; fresh msg
-  shr r8, 5             ; shift msg right 5
-  add r8, r10           ; add key to msg >> 5
-  and r8, r15           ; mask most sig 32 bits
-
-  xor r8, r9            ;
-  xor r11, r8           ; 
-
-  mov r8, r13           ; other half of message
+  
+  cmp rbx, [encrypt]
+  jne dec
   add r8, r11           ; this is encrypted
   and r8, r15           ; mask most sig 32 bits
   mov [rax], r8         ; store it
+  jmp end
+dec:
+  sub r8, r11           ; this is decrypted
+  and r8, r15           ; mask most sig 32 bits
+end:
+  
   mov rdi, pFormat      ; prinf format
   mov rsi, r8           ; what to print
   xor rax, rax          ; printf uses rax for something
